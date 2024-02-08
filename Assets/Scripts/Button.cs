002ac2f2c395695e2b1e3524a844865a55d8d7e0
ZeinabBF <zeba0347@win.dsv.su.se>
2024-02-08 19:10:41 +0100

    Audio
    Audio

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private GameObject bulbLight;
+    private GameObject bulbLight1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulbLight = GameObject.Find("Bulb_01");
+        bulbLight1 = GameObject.Find("Bulb_02");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+    public void Bulb()
+    {
+
+         bulbLight.GetComponent<Renderer>().material.color = new Color(0, 204, 102);
+         bulbLight1.GetComponent<Renderer>().material.color = new Color(0, 204, 102);
+       
+    }
+
+}
